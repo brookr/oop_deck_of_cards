a4c831abba9db255d7a083683788af99fbaf1541
amykangweb <selene6023@gmail.com>
2015-09-01 15:38:22 -0700

    Add README

---
+++ README.md
@@ -0,0 +1,18 @@
+<h1>OOP Deck of Cards</h1>
+
+<h2>Overview</h2>
+
+<p>
+I wanted to be able to create cards with a particular suit and rank and check to see if the card already existed so there wouldn't be duplicate cards in the deck.
+</p>
+
+<h2>Sources</h2>
+
+<p>
+<a href="http://chriskottom.com/blog/2014/10/4-fantastic-ways-to-set-up-state-in-minitest/">http://chriskottom.com/blog/2014/10/4-fantastic-ways-to-set-up-state-in-minitest/</a>
+</p>
+
+<p>
+<a href="http://www.rubyinside.com/a-minitestspec-tutorial-elegant-spec-style-testing-that-comes-with-ruby-5354.html">http://www.rubyinside.com/a-minitestspec-tutorial-elegant-spec-style-testing-that-comes-with-ruby-5354.html</a>
+</p>
+
--- spec/features/deck_spec.rb
+++ spec/features/deck_spec.rb
@@ -1,36 +1,25 @@
-require "spec_helper"
-require "cards"
-#http://www.rubyinside.com/a-minitestspec-tutorial-elegant-spec-style-testing-that-comes-with-ruby-5354.html
+require 'spec_helper'
+require 'cards'
+# http://chriskottom.com/blog/2014/10/4-fantastic-ways-to-set-up-state-in-minitest/
 
-describe "Adding card to deck" do
+describe 'Adding card to deck' do
   before do
     @new_deck = Deck.new
-    @card = Card.new( "Hearts", "12" )
+    @card = Card.new('Hearts', '12')
   end
 
-  it "increases deck size by 1" do
+  it 'increases deck size by 1' do
     @new_deck.add_card(@card)
-    @new_deck.message.must_match( "Card added to deck." )
+    @new_deck.message.must_match('Card added to deck.')
     assert_equal 1, @new_deck.deck.length
   end
 
-  it "when it has 52 cards will not increase size" do
-    [ "clubs", "diamonds", "hearts", "spades" ].each do |obj|
-      13.times do |n|
-        @new_deck.deck << Card.new(obj, n.to_s)
-      end
-    end
+  it 'when card already exists will not add to deck' do
     @new_deck.add_card(@card)
-    @new_deck.message.must_match( "Card not added to deck." )
-    assert_equal 52, @new_deck.deck.length
-  end
-
-  it "when card already exists will not add to deck" do
-    @new_deck.add_card(@card)
-    card2 = Card.new( "Hearts", "12" )
+    card2 = Card.new('Hearts', '12')
     size = @new_deck.deck.length
     @new_deck.add_card(card2)
-    @new_deck.message.must_match( "Card not added to deck." )
+    @new_deck.message.must_match('Card not added to deck.')
     assert_equal size, @new_deck.deck.length
   end
 end
