d68167ffac290394d7bfb49551db286c449c6e1d
amykangweb <selene6023@gmail.com>
2015-09-01 15:36:42 -0700

    Clean up code with rubocop.

--- lib/cards.rb
+++ lib/cards.rb
@@ -1,3 +1,4 @@
+# Create deck and check cards for uniqueness.
 class Deck
   attr_accessor :deck, :message
   def initialize
@@ -6,69 +7,54 @@ class Deck
   end
 
   def add_card(card)
-    unless self.check_deck(card) == true
+    if check_unique(card) == false
+      @message = 'Card not added to deck.'
+    else
       @deck << card
-      @message = "Card added to deck."
-    else
-      @message = "Card not added to deck."
-    end
-  end
-
-  def check_deck(card)
-    if @deck.length == 52 || self.check_unique(card) == true
-      return true
-    else
-      return false
+      @message = 'Card added to deck.'
     end
   end
 
   def check_unique(card)
-    if @deck.select { |obj| obj.suit == card.suit && obj.rank == card.rank }.length != 0
-      return true
-    else
+    if @deck.count do |obj|
+         obj.suit == card.suit && obj.rank == card.rank
+       end != 0
       return false
+    else
+      return true
     end
   end
 end
 
+# Create cards and check to see if it's valid.
 class Card
   attr_accessor :suit, :rank, :message
-  @@suits = [ "clubs", "diamonds", "hearts", "spades" ]
-  @@ranks = { "11" => "jack", "12" => "queen", "13" => "king" }
+  SUITS = %w(clubs diamonds hearts spades)
+  RANKS = { '11' => 'jack', '12' => 'queen', '13' => 'king' }
 
   def initialize(suit, rank)
-    @message = nil
     if Card.check_suit(suit.downcase) == true
       @suit = suit.downcase
     else
-      return @message = "Invalid suit."
+      return @message = 'Invalid suit.'
     end
-    if Card.check_rank(rank) != nil
+    if !Card.check_rank(rank).nil?
       @rank = Card.check_rank(rank)
     else
-      return @message = "Invalid rank."
+      return @message = 'Invalid rank.'
     end
   end
 
   def self.check_suit(suit)
-    @@suits.include?(suit) ? true : false
+    SUITS.include?(suit) ? true : false
   end
 
   def self.check_rank(rank)
-    if @@ranks.include?(rank)
-      return @@ranks[rank]
+    if RANKS.include?(rank)
+      return RANKS[rank]
     elsif (1..10).to_a.include?(rank.to_i)
       return rank
     end
     nil
   end
 end
-
-new_deck = Deck.new
-card = Card.new( "hearts", "12" )
-new_deck.add_card(card)
-p new_deck.message
-card2 = Card.new( "hearts", "12" )
-new_deck.add_card(card2)
-p new_deck.message
-p new_deck.deck
